6c80fbf2f9d3b62f8d81381e70d99660269c0d53
Daniel dos Santos <daniel@automastersat.com.br>
2021-12-09 10:21:30 -0300

    parte02 - enviando o token para a API

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:meetups/http/web.dart';
+import 'package:meetups/models/device.dart';
 import 'package:meetups/screens/events_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +21,26 @@ void main() async {
   runApp(App());
 }
 
-void setPushToken(String? token) async {}
+void setPushToken(String? token) async {
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  String? brand;
+  String? model;
+
+  if (Platform.isAndroid) {
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    print('Rodando no ${androidInfo.model}');
+    brand = androidInfo.brand;
+    model = androidInfo.model;
+  } else {
+    IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+    print('Rodando no ${iosInfo.utsname.machine}');
+    model = iosInfo.utsname.machine;
+    brand = 'Apple';
+  }
+
+  Device device = Device(brand: brand, model: model, token: token);
+  sendDevice(device);
+}
 
 class App extends StatelessWidget {
   @override
